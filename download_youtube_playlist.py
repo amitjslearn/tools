550e7f4d9593f5215f4324368d6b174d2a40c8e9
amitjslearn <amitjslearn@gmail.com>
2022-01-23 21:47:32 +0530

    only audio

--- download_youtube_playlist.py
+++ download_youtube_playlist.py
@@ -1,10 +1,17 @@
 from pytube import Playlist
-playlist = Playlist('https://www.youtube.com/watch?v=wg4dUKMjR1w&list=PLd1t7W2ShsB-EIJVyhaeRdgkO7Ax5Wr_8')
+
+done = [
+    'https://www.youtube.com/watch?v=wg4dUKMjR1w&list=PLd1t7W2ShsB-EIJVyhaeRdgkO7Ax5Wr_8'
+]
+now = 'https://www.youtube.com/playlist?list=PLd1t7W2ShsB8A7PNrvvfaLhVJP-rdG_-l'
+
+playlist = Playlist(now)
 print('Number of videos in playlist: %s' % len(playlist.video_urls))
 
 # Loop through all videos in the playlist and download them
 count = 1
 for video in playlist.videos:
-    video.streams.get_highest_resolution().download()
+    streams = video.streams.filter(only_audio=True).first().download(output_path="/home/amit/Desktop/sounds-of-isha/damaru") # .get_highest_resolution().download()
+    print(streams)
     print('done',count)
     count +=1
